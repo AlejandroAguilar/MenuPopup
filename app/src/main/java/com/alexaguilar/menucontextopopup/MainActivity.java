df6e1e6a1c601a843b7e991f1f8438e55f032c86
Alejandro Aguilar <downvieri@gmail.com>
2016-07-26 23:47:27 -0500

    Menu Popup
    Menu Popup

---
+++ app/src/main/java/com/alexaguilar/menucontextopopup/MainActivity.java
@@ -0,0 +1,44 @@
+package com.alexaguilar.menucontextopopup;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opciones, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.mAbout:
+                    Intent intent = new Intent(this, ActivityAbout.class);
+                    startActivity(intent);
+                break;
+
+            case R.id.mSettings:
+                    Intent i = new Intent(this, ActivitySettings.class);
+                    startActivity(i);
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
